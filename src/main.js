cd0263c22bfdd126290386fc1900c95773501564
c-bd <18334794147@qq.com>
2019-10-11 15:21:56 +0800

    安装依赖 和必要的基础准备工作

--- src/main.js
+++ src/main.js
@@ -2,8 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'amfe-flexible/index.js'
+import {
+  Button,
+  Cell,
+  CellGroup,
+  NavBar,
+  Field,
+  Toast
+} from 'vant'
 Vue.config.productionTip = false
-
+Vue
+  .use(Button)
+  .use(Cell)
+  .use(CellGroup)
+  .use(NavBar)
+  .use(Field)
+  .use(Toast)
 new Vue({
   router,
   store,
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    }
 
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
